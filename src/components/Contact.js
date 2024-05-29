58eccf5fcacbbbbee5a2288109b95cb0f842bb0e
Emre Demir <emred644@gmail.com>
2024-05-29 12:28:28 +0300

    hadi bakalim

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div className="contact">
+      <h1>Contact Me</h1>
+      <p>Email: emre@example.com</p>
+      <p>Phone: +90 123 456 7890</p>
+    </div>
+  );
+}
+
+export default Contact;
